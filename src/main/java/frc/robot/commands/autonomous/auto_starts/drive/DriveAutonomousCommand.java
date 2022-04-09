858809ef44d5d80abf6491a19f3da9b52445929c
SatyamPrakashSingh <52355137+SatyamPrakashSingh@users.noreply.github.com>
2022-04-09 18:16:32 +0530

    Update DriveAutonomousCommand.java

--- src/main/java/frc/robot/commands/autonomous/auto_starts/drive/DriveAutonomousCommand.java
+++ src/main/java/frc/robot/commands/autonomous/auto_starts/drive/DriveAutonomousCommand.java
@@ -21,13 +21,15 @@ public class DriveAutonomousCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-      this.driveSubsystem.arcadeInbuilt(-0.22, 0.0);
+//       this.driveSubsystem.arcadeInbuilt(-0.22, 0.0);
+    this.driveSubsystem.drive(-0.22, -0.22);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    this.driveSubsystem.arcadeInbuilt(0.0, 0.0);
+//     this.driveSubsystem.arcadeInbuilt(0.0, 0.0);
+    this.driveSubsystem.drive(0.0, 0.0);
   }
 
   // Returns true when the command should end.
